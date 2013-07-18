4c878f9337289bd61938c2dd45c2f15c407d065e
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-07-18 02:54:13 +0200

    added chat

--- public/js/main_client.js
+++ public/js/main_client.js
@@ -2,20 +2,60 @@ jQuery(function ($){
     console.log('jQuery est reconnu');
     // Mode strict
     // 'use strict';
+    var socket      = io.connect();
+    var msgtpl      = $('#msgtpl').html();
+    var lastmsg     = false;
+    
+    $('#msgtpl').remove();
 
-    
-    /* Code socket */
-    var socket                              = io.connect();
-    
     socket.on("confirm_connection", function(data){
         $("#receiver").text(data.text);
+        $("#cacheAll").fadeOut();
     });
     
-    $('body').on('click', '#receiver', function(event){
-        socket.emit("clickOnP");
+    $('#loginform').submit(function(event){
+        event.preventDefault();
+        socket.emit('login', {
+            username : $('#username').val()
+        });
     });
-    socket.on("confirm_clickOnP", function(data){
-        $("#receiver").text(data.text);
+    socket.on("logged", function(){
+        $('#login').fadeOut();
+        $('#message').focus();
+    });
+
+    /* Send message to server */
+    $('#form').submit(function(event){
+        event.preventDefault();
+        if($('#message').val() != ''){
+            socket.emit('newmsg', {message: $('#message').val()})
+            $('#message').val('');
+            $('#message').focus();
+        }
+    });
+    
+    /* add a message */
+    socket.on('newmsg', function(message){
+        if(lastmsg != message.user.id){
+            $('#message').append("<div class='sep'></div>");
+            lastmsg = message.user.id;
+        }
+        $('#messages').append('<div class="message">' + Mustache.render(msgtpl, message) + "</div>");
+        $('#chat_output').animate({scrollTop : $('#messages').prop('scrollHeight') }, 50);
+    });
+
+    
+    /***********************
+    * Manage connected users
+    ***********************/
+    /* Connection of a client */
+    socket.on('newusr', function(user){
+        $("#users").append('<p id="' + user.username + '">' + user.username + '</p>');
+    });
+    
+    /* Deconnection of a client */
+    socket.on('disusr', function(user){
+        $('#' + user.username).remove();
     });
 
 });
