13162b2779f293cf91921d3cb6858fcc43f4685c
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-07-18 02:54:43 +0200

    added chat

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /* Modules */
 var colors                              = require("colors");
 var prototypejs                         = require('prototype');
-Object.extend(global, prototypejs); // to facilitate use of prototype
+Object.extend(global, prototypejs);     // to facilitate use of prototype
 var http                                = require("http");
 var url                                 = require("url");
 var fs                                  = require("fs");
@@ -12,13 +12,19 @@ var server                              = http.createServer(app);
 var io                                  = require('socket.io').listen(server);
 
 /* Configuration */
+// For server
 var port                                = 3000;
+// For socket.io
+var users                               = {};
+var messages                            = [];
+var history                             = 10;
 
 /* Create server */
 server.listen( port );
 
 // When we ask for a webpage
 app.use(express.static(__dirname + '/public'));
+
 // When root url is called
 app.get('/', function(req, res) {
     fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text){
@@ -26,18 +32,64 @@ app.get('/', function(req, res) {
     });
 });
 
+console.log( "      Server listening on port : ".blue.bold + port );
+
+
 // Here we play with sockets
 io.sockets.on('connection', function(socket){
-    var data                            = {};
-    data.text                           = "It is working ! Click on me !";
-    console.log( "      Client connected".blue.bold );
-    socket.emit("confirm_connection", data);
 
-    socket.on("clickOnP", function(){
-        var data                        = {};
-        data.text                       = "You clicked, it changed !";
-        socket.emit("confirm_clickOnP", data);
-    })
+    var me = false;
+    console.log('      New user'.blue.bold);
+    
+    for (var k in users){
+        socket.emit('newusr', users[k]);
+    }
+    for (var k in messages){
+        socket.emit('newmsg', messages[k]);
+    }
+
+    socket.emit("confirm_connection", {text : "It is working ! Click on me !"});
+
+    
+    /* We get a message */
+    socket.on('newmsg', function(message){
+        message.user                    = me;
+        date                            = new Date();
+        message.h                       = date.getHours();
+        message.m                       = date.getMinutes();
+        // mise en forme de la date pour rajouter les 0 esthétique qui manquent
+        if (message.h <10){
+            message.h = "0" + message.h;}
+        if (message.m <10){
+            message.m = "0" + message.m;}
+        
+        messages.push(message);
+        
+        if(messages.length > history){
+            messages.shift();
+        }
+        
+        console.log('      A message has been send'.bold.blue);
+        io.sockets.emit('newmsg', message);
+    });
+
+    
+    /* User connect */
+    socket.on('login', function(user){
+        me = user;
+        socket.emit("logged");
+        users[me.id] = me;
+        io.sockets.emit('newusr', me);
+        console.log('      User connected');
+    });
+    
+    /* User leave */
+    socket.on('disconnect', function(){
+        if(!me){
+            return false;
+        }
+        io.sockets.emit('disusr', me);
+        delete users[me.id];
+        console.log('      User disconnected');
+    });
 });
-
-console.log( "      Server listening on port : ".blue.bold + port );
